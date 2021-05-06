d32225edb0f2f090044f4eb27012ca8993e8a4be
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2021-05-06 13:14:05 +0600

    may 6, 2021 3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import db from "../firebase";
 import ImgSlider from "./ImgSlider";
 import Movies from "./Movies";
 import Viewers from "./Viewers";
 
 function Home() {
+  useEffect(() => {
+    db.collection(Movies).onSnapshot((snapshot) => {
+      let tempMovies = snapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+    });
+    return () => {};
+  }, []);
   return (
     <Container>
       <ImgSlider />
